fb53ffc04771d35294b6c3b705bd834bc867b113
zm-garcia <zm.garcia@etribe.mx>
2026-04-22 10:11:06 -0600

    act 22abril

--- 22042026/express/app.js
+++ 22042026/express/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  //res.sendFile("/Users/garzia/PROGRAMACIONWEB2/express/app.js");
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 
 //puerto
-const port = 3000;
+const port = 2000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port http://localhost:${port}`);
 });
