d1b88f140556f52f261c8c86c6e7ba73b1c8c1b7
zm-garcia <zm.garcia@etribe.mx>
2026-04-27 20:57:56 -0600

    tarea express calculadora

--- TAREAS/CALCULADORA/app.js
+++ TAREAS/CALCULADORA/app.js
@@ -1,3 +1,13 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+//Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
 function agregarPantalla(value) {
     document.getElementById("display").value += value;
 }
